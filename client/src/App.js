a73efc93b133fbd71c110d289aa79495f3d8f9b7
charint4 <rafifridhoo@gmail.com>
2021-07-17 19:39:38 +0700

    add frontend and its functionality

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+import './App.css';
+import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import setAuthToken from './utils/setAuthToken';
+import AlertState from './context/alert/AlertState';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  return (
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <PrivateRoute exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
+  );
+};
+
+export default App;
